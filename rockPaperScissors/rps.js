97be592634f71f270ffe8e978a5c1c9cb9272fa8
maxhannan <maxhannan@substanceseven.com>
2021-01-25 03:41:22 -0600

    code refinement

--- rockPaperScissors/rps.js
+++ rockPaperScissors/rps.js
@@ -4,46 +4,69 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
-
-let gamend = false;
-while(gamend === false){
-    let playerMove = prompt('rock, paper or scissors? ').toLowerCase();
-    let compMove = moves[getRandomInt(0,3)];
-    console.log(`Player move: ${playerMove}, Computer move: ${compMove}`);
-    let validAnswer = moves.includes(playerMove);
-    if (validAnswer === false){
-        console.log(`Enter a valid answer! ${'"'+playerMove+'"'} is not one of the options!`);
-        continue;
-    };   
-    if(playerMove === compMove){
-        console.log('A Draw!');
-        continue;
+function computerPlay(list){
+    return moves[getRandomInt(0,list.length)];
+}
+function checkValid(playerMove){
+    return moves.includes(playerMove);
+}
+function playRound(computerSelection, playerSelection){
+    if(playerSelection === computerSelection){
+        return 'A Draw!';
     };
-    if(playerMove === 'rock'){
-        if(compMove === 'paper'){
-            console.log('Computer wins, paper beats rock!');
-            break;
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            return 'Computer wins, paper beats rock!';
         }else{
-            console.log('Player wins, rock beats scissors!');
-            break;
+            return 'Player wins, rock beats scissors!' ;
         };
     };
-    if(playerMove === 'paper'){
-        if(compMove === 'scissors'){
-            console.log('Computer wins, scissors beats paper!');
-            break;
+    if(playerSelection === 'paper'){
+        if(computerSelection === 'scissors'){
+            return 'Computer wins, scissors beats paper!';
         }else{
-            console.log('Player wins, paper beats rock!');
-            break;
+            return 'Player wins, paper beats rock!';
         };
     };
-    if(playerMove === 'scissors'){
-        if(compMove === 'rock'){
-            console.log('Computer wins, rock beats scissors!');
-            break;
+    if(playerSelection === 'scissors'){
+        if(computerSelection === 'rock'){
+            return 'Computer wins, rock beats scissors!';
         }else{
-            console.log('Player wins, scissors beats paper!');
-            break;
+            return 'Player wins, scissors beats paper!';
         };
     };
 };
+function game(rounds){
+    let playerScore = 0;
+    let compScore = 0;
+    let draws = 0;
+    for(let i = 0; i < rounds; i++){
+        let playerMove = prompt('rock, paper or scissors? ').toLowerCase();
+        let compMove = computerPlay(moves);
+        result = playRound(compMove, playerMove);
+        if(checkValid(playerMove) == false){
+            console.log('Not Valid, Computer wins by default.');
+            compScore +=1;
+            continue;
+        }
+        if(result.includes('Player')){
+            playerScore +=1;
+        }else if(result.includes('Computer')){
+            compScore +=1;
+        }else{
+            draws += 1;
+        };
+        console.log(result)};
+    if(playerScore > compScore){
+        console.log(`Player wins! It was ${playerScore} to ${compScore}`);
+        console.log(`There was ${draws} draw(s)`);
+    }else if(playerScore < compScore){
+        console.log(`Computer wins! It was ${compScore} to ${playerScore}`);
+        console.log(`There was ${draws} draw(s)`);
+    }else{
+        console.log(`A Tie! It was ${playerScore} to ${compScore}`);
+        console.log(`There was ${draws} draw(s)`);
+    };
+};
+rounds = prompt('How many rounds would you like to play? ');
+game(parseInt(rounds));
