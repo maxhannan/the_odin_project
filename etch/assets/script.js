064ea05141a79cebebd7aa76d561289326af998e
maxhannan <maxhannan@substanceseven.com>
2021-01-27 14:05:00 -0600

    Making buttons work

--- etch/assets/script.js
+++ etch/assets/script.js
@@ -1,4 +1,5 @@
 function grid(width){
+    
     var main = document.getElementById('main');
     main.style.gridTemplateColumns = `repeat(${width}, 1fr)`
     main.style.gridTemplateRows = `repeat(${width}, 1fr)`
@@ -11,16 +12,50 @@ function grid(width){
         
     }
 }
-let gridSize = prompt('Grid Size:')
-window.onload = grid(gridSize);
+let gridSize = prompt('Grid Size:');
+window.open = grid(gridSize);
 var color = [, "#3C9EE7", "#E7993C",  
 "#E73C99", "#3CE746", "#E7993C"]; 
 
+var main = document.getElementById('main');
+let boxes = document.getElementsByClassName('box');
+
+function newBoard(width){
+    main.innerHTML = ''
+    grid(width);
+    let boxes = document.getElementsByClassName('box');
+    Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
+        v.style.background = color[Math.floor(Math.random() * color.length)];
+      }));
+};
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+async function shake(){
+    main.classList.add('shake'); 
+    await sleep(500);
+    main.classList.remove('shake'); 
+}
+document.getElementsByClassName('gridSize')[0].onclick = function() {
+    gridSize = prompt('Grid Size:');
+    newBoard(gridSize)
+    shake();
+  }
+  
+document.getElementsByClassName('reset')[0].onclick = function() {
+    newBoard(gridSize);
+    shake();
+    
+    
+}
+  
 
 
-var grid = document.getElementsByClassName('box');
 
-Array.from(grid).forEach(v => v.addEventListener('mouseover', function() {
+
+
+Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
   v.style.background = color[Math.floor(Math.random() * color.length)];
 }));
 
