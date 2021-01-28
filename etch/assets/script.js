274b29b9e9d7911eb6a49c09aed2459482f04a58
maxhannan <maxhannan@substanceseven.com>
2021-01-27 23:57:02 -0600

    code cleanup

--- etch/assets/script.js
+++ etch/assets/script.js
@@ -6,31 +6,26 @@ let boxList = Array.from(boxes);
 let gridOn = true;
 let colorPicker = document.getElementById('cp');
 let hex = 'red';
-// color picker
-// var colorPicker = new iro.ColorPicker('#picker');
-colorPicker.addEventListener("input", watchColorPicker, false);
-
-function watchColorPicker(event) {
-    hex = event.target.value;
-    console.log(hex);
-    
-    colorPick();
-
-}
-Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
-    v.style.background = 'black';
-  }));
-
+blackPick()
+// Random Whole Number Generator 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); 
 }
+// Set up event listener for color picker
+colorPicker.addEventListener("input", watchColorPicker, false);
+function watchColorPicker(event) {
+    hex = event.target.value;
+    console.log(hex);
+    colorPick();
+}
+// Creates grid of boxes and appends to DOM, emptys and resets if called again. 
 function grid(width){
     var main = document.getElementById('main');
     main.style.gridTemplateColumns = `repeat(${width}, 1fr)`
     main.style.gridTemplateRows = `repeat(${width}, 1fr)`
-    
+    main.innerHTML = ''
     for (var i=0; i<width; i++) {
         for (var j=0; j<width; j++) {
         var box = document.createElement("div");
@@ -41,31 +36,22 @@ function grid(width){
     } 
     
 }
-
+// Random Color Picker
 function randColor(){
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 } 
 
-function newBoard(width){
-    main.innerHTML = ''
-    grid(width);
-    let boxes = document.getElementsByClassName('box');
-    Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
-        v.style.background = 'black';
-      }));
-
-};
-
+// time sleep function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
+}
+// Turns on and off shake animation for etch
 async function shake(){
     main.classList.add('shake'); 
     await sleep(500);
     main.classList.remove('shake'); 
 }
-
+// Gets user input for new grid size, and calls reset functions
 function gridChange() {
     let validInput = false;
     gridSize = prompt('Grid Size (2-100)');
@@ -80,16 +66,10 @@ function gridChange() {
             continue;
         }
     }
-    if(gridSize < 2 || gridSize > 100){
-
-    }
-    newBoard(gridSize)
-    shake();
-    gridReverse()
-    gridTog() 
-    blackPick()
+    reset()
 }
 
+// toggles borders for boxes on and off 
 function gridTog() {
     let boxList = Array.from(boxes)
     if(gridOn === true){
@@ -107,6 +87,7 @@ function gridTog() {
     }
 
 }
+// reverse function for reset 
 function gridReverse(){
     if(gridOn === true){
         gridOn = false;
@@ -115,13 +96,15 @@ function gridReverse(){
     }
 }
 
+// Calls all reset functions
 function reset() {
-    newBoard(gridSize);
+    grid(gridSize);
     shake();
     gridReverse()
     gridTog() 
     blackPick() 
 }
+// Functions for etch a sketch tools. 
 function colorPick() {
     Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
         v.style.background = hex;
@@ -143,9 +126,7 @@ function randomPick() {
 
 function blackPick() {
     Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
-
-        v.style.background = 'black';
-        
+        v.style.background = 'black';  
       })); 
       document.querySelector('#randcolor').style.backgroundColor = 'black'; 
       document.querySelector('#eraser').style.backgroundColor = 'black'; 
@@ -154,13 +135,12 @@ function blackPick() {
 }
 function eraser() {
     Array.from(boxes).forEach(v => v.addEventListener('mouseover', function() {
-
         v.style.background = 'transparent';
-        
       })); 
       document.querySelector('#randcolor').style.backgroundColor = 'black'; 
       document.querySelector('#cp').style.opacity = .4; 
       document.querySelector('#black').style.backgroundColor = 'black';   
       document.querySelector('#eraser').style.backgroundColor = 'green';    
 }
+
 
